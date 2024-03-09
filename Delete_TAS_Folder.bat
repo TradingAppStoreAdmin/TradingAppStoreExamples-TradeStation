bd3706d592057c47665e0c5bcee8b298e76bc4b1
TradingAppStoreAdmin <116606022+TradingAppStoreAdmin@users.noreply.github.com>
2024-03-09 16:45:44 -0500

    Add files via upload

--- Delete_TAS_Folder.bat
+++ Delete_TAS_Folder.bat
@@ -1,2 +1,5 @@
 @echo off
-rmdir /s /q C:\ProgramData\TradingAppStore
+rmdir /s /q C:\ProgramData\TradingAppStore\Licenses\
+rmdir /s /q C:\ProgramData\TradingAppStore\Logs\
+echo Deleted all temp licenses. DO NOT DELETE TEMP FOLDER.
+pause
